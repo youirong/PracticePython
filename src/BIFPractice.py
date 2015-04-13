3546a964c77d881fcd3ae54eb3b42dc4c810fb91
youirong <398088654@qq.com>
2015-04-13 20:46:17 +0800

    add a new parameter

--- src/BIFPractice.py
+++ src/BIFPractice.py
@@ -10,15 +10,16 @@ Created on 2015年4月13日
 #range() 迭代数字，类似于控制循环。好像在python里面，就不太用while了呀！
 movies = ["1", "2", ["3", ["4", "5"]]]
 
-def print_lol(the_list, level=0):
+def print_lol(the_list, indent=False, level=0):
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level+1)
+            print_lol(each_item, indent, level+1)
         else:
-            for tap_stop in range(level):
-                print("\t", end=" ")
+            if indent:
+                for tap_stop in range(level):
+                    print("\t", end=" ")
             print(each_item)
 #             print('>>>')
 #             print(level)
 
-print_lol(movies,-9)
+print_lol(movies, True,0)
